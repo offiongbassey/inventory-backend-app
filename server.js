bb3648d2997313c92f14ed9b30a62912741a932d
naijaspeed1 <naijaspeed1@gmail.com>
2023-05-02 12:29:24 +0100

    render.com deployment issue

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors"); 
-const app = express();
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
@@ -11,42 +10,41 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const contactRoute = require("./routes/contactRoute");
 
-const uri = 
-"mongodb+srv://ntplus:08096097539@cluster0.x3ryd.mongodb.net/Pinvent-app?retryWrites=true&w=majority";
-//Middlewares
+const app = express();
+
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({
-    origin: ["http://localhost:3000", "https://pinventori-app.vercel.app"], 
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinventory-app.vercel.app"],
     credentials: true,
-}));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+  })
+);
 
-//Routes middleware
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes Middleware
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-app.use("/api/contact", contactRoute);
-//Routes
+app.use("/api/contactus", contactRoute);
+
+// Routes
 app.get("/", (req, res) => {
-    res.send("Home Page");
-
+  res.send("Backend Connected");
 });
-//error middleware
+
+// Error Middleware
 app.use(errorHandler);
-
-async function connect() {
-    try{
-        await mongoose.connect(uri);
-        console.log("Connected to MongoDB");
-
-    }catch(error){
-        console.log(error);
-    }
-}
-
-connect();
-app.listen(5000, () => {
-    console.log("Server started at port 5000");
-});
+// Connect to DB and start server
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
